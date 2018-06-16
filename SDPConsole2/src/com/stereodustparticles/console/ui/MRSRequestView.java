1413e9bbb92fd75bda2a8e3e0f1617f4737c226c
backerm3 <backerm3@u.rochester.edu>
2018-06-16 03:41:23 -0400

    Create action bar

--- SDPConsole2/src/com/stereodustparticles/console/ui/MRSRequestView.java
+++ SDPConsole2/src/com/stereodustparticles/console/ui/MRSRequestView.java
@@ -15,7 +15,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -37,7 +40,18 @@ public class MRSRequestView {
 		reqList.setItems(MRSIntegration.getRequestList());
 		root.getChildren().add(reqList);
 		
-		// Buttons
+		// Action Selector
+		HBox actionSelector = new HBox(8);
+		actionSelector.setAlignment(Pos.CENTER_LEFT);
+		ChoiceBox<String> action = new ChoiceBox<String>();
+		action.getItems().addAll("Queue", "Queue & Load to Deck 1", "Queue & Load to Deck 2", "Queue & Add Tentative", "Mark Played", "Decline");
+		TextField comment = new TextField();
+		comment.setPromptText("Comment");
+		Button go = new Button("Go");
+		actionSelector.getChildren().addAll(new Label("With selected:"), action, comment, go);
+		root.getChildren().add(actionSelector);
+		
+		// More Buttons
 		HBox lowerButts = new HBox(10);
 		Button refresh = new Button("Refresh List");
 		refresh.setOnAction((e) -> MRSIntegration.refresh());
