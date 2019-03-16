461c257ab936b4e5210c04b76490f0ee55dc55c5
backerm3 <backerm3@u.rochester.edu>
2019-03-16 00:34:42 -0400

    Set deck faders to 0 on no ReplayGain

--- SDPConsole2/src/com/stereodustparticles/console/ui/MoolerCasterView.java
+++ SDPConsole2/src/com/stereodustparticles/console/ui/MoolerCasterView.java
@@ -401,10 +401,8 @@ public class MoolerCasterView {
 				DeckLoadRequest req = new DeckLoadRequest(cEntry.getTitle(), cEntry.getArtist(), cEntry.getDuration(), cLibrary, LibraryManager.getLibraryForName(cLibrary).getPathInLibrary(cEntry), 1);
 				EventBus.fireEvent(new Event(EventType.DECK_REQUEST_LOAD, req));
 				
-				// If ReplayGain is present, adjust the volume fader to its value
-				if ( cEntry.getGain() != 0.0f ) {
-					EventBus.fireEvent(new Event(EventType.DECK_FADER_SET, 1, cEntry.getGain()));
-				}
+				// Adjust the volume fader to the ReplayGain value, or 0 dB if none is present
+				EventBus.fireEvent(new Event(EventType.DECK_FADER_SET, 1, cEntry.getGain()));
 				
 				if ( Prefs.loadBoolean(Prefs.AUTO_ADD_TENTATIVE) && ! Decks.snpIsEnabled() ) {
 					EventBus.fireEvent(new Event(EventType.PLAYLIST_ADD, new PlaylistEntry(cEntry, true, LibraryManager.getLibraryForName(cLibrary).getDefaultFlags())));
@@ -428,10 +426,8 @@ public class MoolerCasterView {
 				DeckLoadRequest req = new DeckLoadRequest(cEntry.getTitle(), cEntry.getArtist(), cEntry.getDuration(), cLibrary, LibraryManager.getLibraryForName(cLibrary).getPathInLibrary(cEntry), 2);
 				EventBus.fireEvent(new Event(EventType.DECK_REQUEST_LOAD, req));
 				
-				// If ReplayGain is present, adjust the volume fader to its value
-				if ( cEntry.getGain() != 0.0f ) {
-					EventBus.fireEvent(new Event(EventType.DECK_FADER_SET, 2, cEntry.getGain()));
-				}
+				// Adjust the volume fader to the ReplayGain value, or 0 dB if none is present
+				EventBus.fireEvent(new Event(EventType.DECK_FADER_SET, 2, cEntry.getGain()));
 				
 				if ( Prefs.loadBoolean(Prefs.AUTO_ADD_TENTATIVE) && ! Decks.snpIsEnabled() ) {
 					EventBus.fireEvent(new Event(EventType.PLAYLIST_ADD, new PlaylistEntry(cEntry, true, LibraryManager.getLibraryForName(cLibrary).getDefaultFlags())));
